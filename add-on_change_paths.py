64189e0a8a0fe3e187f847d139c2c9babb6db16e
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2020-05-14 21:01:35 +0200

    Update add-on_change_paths.py

--- add-on_change_paths.py
+++ add-on_change_paths.py
@@ -2,44 +2,76 @@ bl_info = {
     'name' : 'Change Paths',
     'author' : 'Hans Willem Gijzel',
     'version' : (1, 0),
-    'blender' : (2, 80, 0  ),
-    'location' : 'View 3D > Tools > My Addon',
+    'blender' : (2, 83, 0  ),
+    'location' : 'Properties Panel > Output > Change Paths',
     'description' : 'Changes output paths',
     'warning' : '',
     'wiki_url' : '',
     'category' : 'rendering'
     }
 
-
 #imports
 import bpy
 import os.path
+import subprocess
 
-#the main functions
-def main_change_paths(s1, s2):
+def main_check_mismatch():
+    if os.path.exists(r'C:\Users\hansw') and r'C:\Users\polpot' in bpy.context.scene.render.filepath:
+        return True
+    if os.path.exists(r'C:\Users\polpot') and r'C:\Users\hansw' in bpy.context.scene.render.filepath:
+        return True
+    else:
+        return False
+
+def main_check_node_mismatch():
+    if not bpy.context.scene.use_nodes:
+        return False
+    else:
+        for i in bpy.context.scene.node_tree.nodes:
+            if i.type == 'OUTPUT_FILE':
+                if i.base_path != os.path.dirname(bpy.context.scene.render.filepath):
+                    return True
+                    break
+                else:
+                    return False
+
+def main_get_pc():
+    if os.path.exists(r'C:\Users\hansw'):
+        return 'Home'
+    if os.path.exists(r'C:\Users\polpot'):
+        return 'Redrum'
+
+def main_change_paths():
+    if os.path.exists(r'C:\Users\hansw'):
+        s1 = r'C:\Users\polpot'
+        s2 = r'C:\Users\hansw'
+    if os.path.exists(r'C:\Users\polpot'):
+        s1 = r'C:\Users\hansw'
+        s2 = r'C:\Users\polpot'   
+
     bpy.context.scene.render.filepath = bpy.context.scene.render.filepath.replace(s1, s2)
     # file output nodes
     for i in bpy.context.scene.node_tree.nodes:
         if i.type == 'OUTPUT_FILE':
             i.base_path = i.base_path.replace(s1, s2)
 
+#update the file output node paths bases on the render path
 def main_update_file_output_nodes():
     for i in bpy.context.scene.node_tree.nodes:
         if i.type == 'OUTPUT_FILE':
             i.base_path = os.path.dirname(bpy.context.scene.render.filepath)
 
+def main_open_folder():
+    subprocess.Popen('explorer ' + os.path.dirname(bpy.context.scene.render.filepath))
 
 
 #operator class
-class SCRIPT_OT_change_paths(bpy.types.Operator):
+class SCRIPT_OT_update_paths(bpy.types.Operator):
     #operator attributes
     """Tooltip"""
-    bl_label = 'Change Paths'
-    bl_idname = 'script.change_paths'
+    bl_label = 'Update File Paths'
+    bl_idname = 'script.update_paths'
 
-    pc_path_01: bpy.props.StringProperty(name = 'pc_path_01')
-    pc_path_02: bpy.props.StringProperty(name = 'pc_path_02')
-    
     #poll - if the poll function returns False, the button will be greyed out
     @classmethod
     def poll(cls, context):
@@ -47,15 +79,15 @@ class SCRIPT_OT_change_paths(bpy.types.Operator):
     
     #execute
     def execute(self, context):
-        main_change_paths(self.pc_path_01, self.pc_path_02)
+        main_change_paths()
         return {'FINISHED'}
 
 #operator class
-class SCRIPT_OT_update_file_output_nodes(bpy.types.Operator):
+class SCRIPT_OT_update_nodes(bpy.types.Operator):
     #operator attributes
     """Tooltip"""
     bl_label = 'Update File Output Nodes'
-    bl_idname = 'script.update_file_output_nodes'
+    bl_idname = 'script.update_nodes'
 
     #poll - if the poll function returns False, the button will be greyed out
     @classmethod
@@ -67,6 +99,22 @@ class SCRIPT_OT_update_file_output_nodes(bpy.types.Operator):
         main_update_file_output_nodes()
         return {'FINISHED'}
 
+#operator class
+class SCRIPT_OT_open_folder(bpy.types.Operator):
+    #operator attributes
+    """Tooltip"""
+    bl_label = 'Open Folder'
+    bl_idname = 'script.open_folder'
+
+    #poll - if the poll function returns False, the button will be greyed out
+    @classmethod
+    def poll(cls, context):
+        return 2 > 1
+    
+    #execute
+    def execute(self, context):
+        main_open_folder()
+        return {'FINISHED'}
 
 #panel class
 class VIEW_3D_PT_change_paths(bpy.types.Panel):
@@ -81,26 +129,26 @@ class VIEW_3D_PT_change_paths(bpy.types.Panel):
     #draw loop
     def draw(self, context):
         layout = self.layout
+        layout.label(text = f'PC: {main_get_pc()}')
         col = layout.column(align = True)
-        row = layout.row(align = True)
 
-        props = row.operator('script.change_paths', text='PC Redrum',)
-        props.pc_path_01 = r'C:\Users\hansw'
-        props.pc_path_02 = r'C:\Users\polpot'
+        if main_check_mismatch():
+            btn_text = 'Switch PC'
+            col.operator('script.update_paths', text=btn_text, icon='ERROR')
+  
+        if main_check_node_mismatch():
+            col.operator('script.update_nodes', text='Update Nodes', icon='ERROR')
 
-        props = row.operator('script.change_paths', text='PC Home',)
-        props.pc_path_01 = "polpot"
-        props.pc_path_02 = "hansw"
-
-        row.operator('script.update_file_output_nodes', text='Update Nodes')
+        col.operator('script.open_folder', text='Open Render Folder', icon='FILE_FOLDER')
 
    
 #registration
 classes = (
     VIEW_3D_PT_change_paths,
-    SCRIPT_OT_change_paths,
-    SCRIPT_OT_update_file_output_nodes
-)
+    SCRIPT_OT_update_paths,
+    SCRIPT_OT_update_nodes,
+    SCRIPT_OT_open_folder,
+    )
 
 def register():
     from bpy.utils import register_class
